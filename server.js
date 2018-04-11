f08aea0eed3b3ab9a0e1e7a2275853a7398b9fa9
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-04-11 18:05:12 +0100

    Add GET /todos/:id

--- server.js
+++ server.js
@@ -10,22 +10,38 @@ let app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
+    let todo = new Todo({
         text: req.body.text
     });
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
+    todo.save((err) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {    
+            res.send(todo);
+        }
     });
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send(todos);
-    }, (e) => {
-        res.status(400).send(e);
-    }) ;
+    Todo.find({}, (err, todo) => {
+        if(err) {
+            res.status(400).send(err);
+        }else {
+            res.send(todo);
+        }
+    });
+});
+
+app.get('/todos/:id', (req,res) => {
+    let id = req.params.id;
+
+    Todo.findById(id, (err, todo) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.send(todo);
+        }
+    })
 });
 
 app.listen(3000, () => {
